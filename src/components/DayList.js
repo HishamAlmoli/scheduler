c47e0ea3dff3efcef28e4a71c01851b64f157303
HishamAlmoli <halmoli@my.centennialcollege.ca>
2023-02-10 18:03:22 -0500

    DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,20 +1,24 @@
-// Imports
+// Imports part
 import React from "react";
 import DayListItem from "./DayListItem";
 
 // Daylist function
-export default function DayList(props) {
-  const { days } = props;
-  const dayListData = days.map((day) => {
+function DayList(props) {
+  const itemList = props.days.map((day) => {
+    
     return (
-      <DayListItem
-        key={day.id}
-        name={day.name}
-        spots={day.spots}
-        selected={day.name === props.day}
-        setDay={props.setDay}
-      />
-    );
-  });
-  return <ul>{dayListData}</ul>;
+    <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.onChange}
+    />
+    )
+  })
+
+  return (
+    <ul> {itemList} </ul>
+  )
 }
+export default DayList;
