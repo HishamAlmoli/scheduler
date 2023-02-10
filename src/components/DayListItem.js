089d3334500e1f2dc62d03b67bb048b0e0a1857c
HishamAlmoli <halmoli@my.centennialcollege.ca>
2023-02-10 18:15:41 -0500

    DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,33 +1,34 @@
-// Imports
+// Imports part
 import React from "react";
 import classnames from "classnames";
 import "components/DayListItem.scss";
 
 // DayListItem function
 export default function DayListItem(props) {
-  const dayListItemClass = classnames("day-list__item", {
+  const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0,
+    "day-list__item--full": props.spots === 0
   });
 
   // FormatSpots function
-  const formatSpots = function () {
-    const numOfSpots = props.spots;
-    if (numOfSpots === 0) return "no spots remaining";
-    else if (numOfSpots === 1) return "1 spot remaining";
-    else {
-      return `${numOfSpots} spots remaining`;
+  function formatSpots() {
+    if (props.spots === 0) {
+     return " No spots remaining ";
     }
-  };
+    if (props.spots === 1) {
+      return " 1 spot remaining ";
+     }
+    return ` ${props.spots} spots remaining `;
+  }
+
   return (
-    <li
-      className={dayListItemClass}
-      selected={props.selected}
-      onClick={() => props.setDay(props.name)}
-      data-testid="day"
-    >
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{formatSpots()}</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected} data-testid="day">
+      <h2 className="text--regular">
+        {props.name}
+      </h2> 
+      <h3 className="text--light">
+        {formatSpots()}
+      </h3>
     </li>
   );
 }
