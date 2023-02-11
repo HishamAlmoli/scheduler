39740a13a86340a5ab39fb0901a180446671a0dc
HishamAlmoli <halmoli@my.centennialcollege.ca>
2023-02-11 16:07:00 -0500

    Hooks functions

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,120 +1,86 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function useApplicationData() {
+const useApplicationData = () => {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {},
-    interviewers: {},
+    interviewers: {}
   });
 
   //This function will set the day inside the useState above
-  const setDay = (day) => {
-    return setState({ ...state, day });
-  };
+  const setDay = day => setState(prev => ({ ...prev, day }));
 
   useEffect(() => {
-    const dayURL = "http://localhost:8001/api/days";
-    const appointmentURL = "http://localhost:8001/api/appointments";
-    const interviewersURL = "http://localhost:8001/api/interviewers";
     Promise.all([
-      axios.get(dayURL),
-      axios.get(appointmentURL),
-      axios.get(interviewersURL),
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then((all) => {
-      setState((prev) => ({
+      setState(prev => ({
         ...prev,
         days: all[0].data,
         appointments: all[1].data,
-        interviewers: all[2].data,
+        interviewers: all[2].data
       }));
+      
     });
   }, []);
 
-  //find the day
-  function findDay(day) {
-    const daysOfWeek = {
-      Monday: 0,
-      Tuesday: 1,
-      Wednesday: 2,
-      Thursday: 3,
-      Friday: 4,
-    };
-    return daysOfWeek[day];
+  const updatedDays = (appointments, appointmentId) => {
+    const foundDay = state.days.find(day => day.appointments.includes(appointmentId))
+    const spots = foundDay.appointments.filter(id => appointments[id].interview === null).length
+
+    return state.days.map(day => day.appointments.includes(appointmentId) ? { ...day, spots } : day)
   }
 
   //function to book an interview
   function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
-      interview: { ...interview },
+      interview: { ...interview }
     };
-
     const appointments = {
       ...state.appointments,
-      [id]: appointment,
+      [id]: appointment
     };
-
-    const dayOfWeek = findDay(state.day);
-    let day = {
-      ...state.days[dayOfWeek],
-      spots: state.days[dayOfWeek],
-    };
-
-    if (!state.appointments[id].interview) {
-      day = {
-        ...state.days[dayOfWeek],
-        spots: state.days[dayOfWeek].spots - 1,
-      };
-    } else {
-      day = {
-        ...state.days[dayOfWeek],
-        spots: state.days[dayOfWeek].spots,
-      };
-    }
-
-    let days = state.days;
-    days[dayOfWeek] = day;
-
-    const url = `/api/appointments/${id}`;
-    return axios.put(url, appointment).then(() => {
-      setState({ ...state, appointments, days });
-    });
+    
+    return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
+      setState({
+        ...state,
+        appointments,
+        days: updatedDays(appointments, id)
+      });
+      
+    })
   }
 
   //function to cancel the interview
-  function cancelInterview(id) {
+  const cancelInterview = id => {
     const appointment = {
       ...state.appointments[id],
-      interview: null,
+      interview: null
     };
     const appointments = {
       ...state.appointments,
-      [id]: appointment,
+      [id]: appointment
     };
 
-    const dayOfWeek = findDay(state.day);
-
-    const day = {
-      ...state.days[dayOfWeek],
-      spots: state.days[dayOfWeek].spots + 1,
-    };
-
-    let days = state.days;
-    days[dayOfWeek] = day;
-
-    const url = `http://localhost:8001/api/appointments/${id}`;
-
-    return axios.delete(url, appointment).then(() => {
-      setState({ ...state, appointments, days });
+    return axios.delete(`/api/appointments/${id}`).then(() => {
+      setState({
+        ...state,
+        appointments,
+        days: updatedDays(appointments, id)
+      });
     });
-  }
-
+  };
+  
   return {
     state,
     setDay,
     bookInterview,
     cancelInterview,
-  };
-}
+  }
+};
+export default useApplicationData;
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,28 +1,23 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  //I used let instead of const
-  let [mode, setMode] = useState(initial);
-  let [history, setHistory] = useState([initial]);
+  const [history, setHistory] = useState([initial]);
 
-  //transition function
-  function transition(nextMode, replace = false) {
+   // transition function
+   function transition(newMode, replace = false) {
     if (replace) {
-      setHistory((prev) => prev.slice(0, -1));
-      setHistory((prev) => [...prev, nextMode]);
-    } else {
-      setHistory((prev) => [...prev, nextMode]);
+      setHistory((prev) => [...prev.slice(0, -1), newMode]);
+      return
     }
-    setMode(nextMode);
+    setHistory((prev) => [...prev, newMode]);
   }
 
-  //back function
+  // back function
   function back() {
-    if (history.length > 1) {
-      setHistory(history.slice(0, -1));
-      setMode(history[history.length - 2]);
+    if (history.length >= 2) {
+      setHistory((prev) => [...prev.slice(0, -1)]);
     }
   }
 
-  return { mode, transition, back };
+  return { mode: history[history.length-1], transition, back };
 }
