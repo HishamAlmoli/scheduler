110c17cc77a4a2bcd6ba02ff6785668d172a91d4
HishamAlmoli <halmoli@my.centennialcollege.ca>
2023-02-11 11:44:57 -0500

    Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-// Imports
+// Imports Part
 import React from "react";
 import useApplicationData from "hooks/useApplicationData";
 import "components/Application.scss";
@@ -12,26 +12,30 @@ import {
 
 // Application function
 export default function Application(props) {
-  const { state, setDay, bookInterview, cancelInterview } =
-    useApplicationData();
-console.log("state ",state, " state.day: ",state.day);
-  let appointments = getAppointmentsForDay(state, state.day);
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
-  const schedule = appointments.map((appointment) => {
-    const interview = getInterview(state, appointment.interview);
-    const interviewers = getInterviewersForDay(state, state.day);
-    return (
-      <Appointment
-        key={appointment.id}
-        id={appointment.id}
-        time={appointment.time}
-        interview={interview}
-        interviewers={interviewers}
-        bookInterview={bookInterview}
-        cancelInterview={cancelInterview}
-      />
-    );
-  });
+  const interviewers = getInterviewersForDay(state, state.day);
+  const appointments = getAppointmentsForDay(state, state.day).map(
+    appointment => {
+      const interview = getInterview(state, appointment.interview);
+        return (
+          <Appointment
+            key={appointment.id}
+            id={appointment.id}
+            time={appointment.time}
+            interview={interview} 
+            interviewers={interviewers}
+            bookInterview={bookInterview}
+            cancelInterview={cancelInterview}
+        />)
+    
+    }
+  );
 
   return (
     <main className="layout">
@@ -43,7 +47,7 @@ console.log("state ",state, " state.day: ",state.day);
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={state.days} day={state.day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -52,8 +56,10 @@ console.log("state ",state, " state.day: ",state.day);
         />
       </section>
       <section className="schedule">
-        {schedule}
-        <Appointment key="last" time="5pm" />
+        <section className="schedule">
+          {appointments}
+          <Appointment key="last" time="5pm" />
+        </section>
       </section>
     </main>
   );
